ba8248531d429928b0d3622e34a8b67b6971f36f
Nicholas Knox <nknox7606@gmail.com>
2025-02-03 13:44:33 -0500

    feat: change the property

--- src/components/PayPalButton.jsx
+++ src/components/PayPalButton.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
+// Renders errors or successfull transactions on the screen.
 function Message({ content }) {
-    return <p>{content}</p>
+    return <p>{content}</p>;
 }
 
-function PayPalButton() {
+function App() {
     const initialOptions = {
         "client-id": "test",
         "enable-funding": "venmo",
@@ -17,112 +18,24 @@ function PayPalButton() {
         "data-sdk-integration-source": "developer-studio",
     };
 
-    const [message, setMessage] = useState("");
+    const displayOnly = ["vaultable"];
 
     return (
         <div className="App">
             <PayPalScriptProvider options={initialOptions}>
                 <PayPalButtons
                     style={{
-                        shape: "rect",
+                        shape: "pill",
                         layout: "vertical",
-                        color: "gold",
+                        color: "blue",
                         label: "paypal",
                     }}
-                    createOrder={async () => {
-                        try {
-                            const response = await fetch("/api/orders", {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                // use the "body" param to optionally pass additional order information
-                                // like product ids and quantities
-                                body: JSON.stringify({
-                                    cart: [
-                                        {
-                                            id: "YOUR_PRODUCT_ID",
-                                            quantity: "YOUR_PRODUCT_QUANTITY",
-                                        },
-                                    ],
-                                }),
-                            });
-
-                            const orderData = await response.json();
-
-                            if (orderData.id) {
-                                return orderData.id;
-                            } else {
-                                const errorDetail = orderData?.details?.[0];
-                                const errorMessage = errorDetail
-                                    ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
-                                    : JSON.stringify(orderData);
-
-                                throw new Error(errorMessage);
-                            }
-                        } catch (error) {
-                            console.error(error);
-                            setMessage(
-                                `Could not initiate PayPal Checkout...${error}`
-                            );
-                        }
-                    }}
-                    onApprove={async (data, actions) => {
-                        try {
-                            const response = await fetch(
-                                `/api/orders/${data.orderID}/capture`,
-                                {
-                                    method: "POST",
-                                    headers: {
-                                        "Content-Type": "application/json",
-                                    },
-                                }
-                            );
-
-                            const orderData = await response.json();
-                            // Three cases to handle:
-                            //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-                            //   (2) Other non-recoverable errors -> Show a failure message
-                            //   (3) Successful transaction -> Show confirmation or thank you message
-
-                            const errorDetail = orderData?.details?.[0];
-
-                            if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
-                                // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-                                // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
-                                return actions.restart();
-                            } else if (errorDetail) {
-                                // (2) Other non-recoverable errors -> Show a failure message
-                                throw new Error(
-                                    `${errorDetail.description} (${orderData.debug_id})`
-                                );
-                            } else {
-                                // (3) Successful transaction -> Show confirmation or thank you message
-                                // Or go to another URL:  actions.redirect('thank_you.html');
-                                const transaction =
-                                    orderData.purchase_units[0].payments
-                                        .captures[0];
-                                setMessage(
-                                    `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`
-                                );
-                                console.log(
-                                    "Capture result",
-                                    orderData,
-                                    JSON.stringify(orderData, null, 2)
-                                );
-                            }
-                        } catch (error) {
-                            console.error(error);
-                            setMessage(
-                                `Sorry, your transaction could not be processed...${error}`
-                            );
-                        }
-                    }}
+                    displayOnly={displayOnly}
+                    fundingSource="paypal"
                 />
             </PayPalScriptProvider>
-            <Message content={message} />
         </div>
     );
 }
 
-export default PayPalButton;
+export default App; 
